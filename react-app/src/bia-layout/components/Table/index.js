05d16df5b98d4741bae838c75cc9e24b3cef8b69
Fabien Karsegard <fabien@karsegard.ch>
2021-08-05 20:19:26 +0200

    ctd

--- react-app/src/bia-layout/components/Table/index.js
+++ react-app/src/bia-layout/components/Table/index.js
@@ -83,9 +83,9 @@ export default props => {
     } = tableInstance);
     return (<>
             <table className={cEx([__base_class])} {...getTableProps()} {...rest}>
-                <thead>
+                <thead  >
                     {headerGroups.map((headerGroup,idx) => (
-                        <>
+                       
                             <tr key={idx} {...headerGroup.getHeaderGroupProps()}>
                                 {headerGroup.headers.map((column,idx) => (
                                     <th
@@ -100,7 +100,7 @@ export default props => {
                                 ))}
                             </tr>
 
-                        </>
+                       
                     ))}
                 </thead>
                 <tbody {...getTableBodyProps()}>
