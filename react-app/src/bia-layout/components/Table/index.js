fa72e622e112ef3696531e47bed2e9b6b1103187
Fabien Karsegard <fabien@karsegard.ch>
2021-08-25 06:59:07 +0200

    hide search pagination when not needed

--- react-app/src/bia-layout/components/Table/index.js
+++ react-app/src/bia-layout/components/Table/index.js
@@ -131,7 +131,7 @@ export default props => {
             </tbody>
 
         </table>
-        <Pagination {...pageProps} />
+        {pageProps.pageCount > 1 && <Pagination {...pageProps} />}
     </LayoutFlexColumn>
     )
 }
