be688ec728ed72d7bcc51ede8a1f68ad815df3f3
Fabien Karsegard <fabien@karsegard.ch>
2021-08-29 18:44:11 +0200

    Update preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -1,14 +1,26 @@
-
+const {camelize} = require('@karesgard/composite-js')
 const { contextBridge, ipcRenderer } = require('electron')
 console.log('hello from preload')
 
 
+const clientAddListener = channel => callback =>  ipcRenderer.on(channel,callback)
+const clientRemoveListener = channel => callback => ipcRenderer.removeListener(channel,callback);
+
+const clientEvent = (key,channel) => ({
+    [`revoke${camelize(key)}`]: clientRemoveListener(channel),
+    [`handle${camelize(key)}`]: clientAddListener(channel),
+
+})
+
+const invokeOnMainProcess = channel => (...args) =>  ipcRenderer.invoke(channel,...args)
+
 contextBridge.exposeInMainWorld(
     'electron',
     {
-      handleOpenFile: callback => ipcRenderer.on('file-open',callback),
-      handleSaveRequest: callback => ipcRenderer.on('trigger-save',callback),
-      save: (filename,data)=> ipcRenderer.invoke('file-save',filename,data)
+      handleOpenFile: clientListen('file-open'),
+      handleSaveRequest: clientListen('trigger-save'),
+      handleLocationChange :clientListen('location-change') ,
+      save:invokeOnMainProcess('file-save')
     }
   )
 
