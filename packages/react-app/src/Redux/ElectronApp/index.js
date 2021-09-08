6546ce6077a45b1d1dfb3a3bfbd7888d1c1219ec
Fabien Karsegard <fabien@karsegard.ch>
2021-09-08 09:01:17 +0200

    Update index.js and submodules.js

--- packages/react-app/src/Redux/ElectronApp/index.js
+++ packages/react-app/src/Redux/ElectronApp/index.js
@@ -7,19 +7,12 @@ import makeActionTypes from './types';
 import makeActions from './actions';
 import makeReducer from './reducer';
 import makeSelectors from './selectors';
-
+import makeSubmodules from './submodules';
 
 
 export default createModule(
     {
-        submodules: getModule => {
-            const { baseSelector, prefix } = getModule();
-            return {
-                pathological_groups: ItemListModule(compose(state => state.options.pathological_groups.data, baseSelector), `${prefix}_PATHO`),
-                ethno_groups: ItemListModule(compose(state => state.options.ethno_groups.data, baseSelector), `${prefix}_ETHNO`),
-                genders: ItemListModule(compose(state => state.options.genders.data, baseSelector), `${prefix}_GENDER`)
-            }
-        },
+        submodules: makeSubmodules,
         selectors: makeSelectors,
         action_types: getModule => makeActionTypes(getModule().prefix),
         actions: makeActions,
