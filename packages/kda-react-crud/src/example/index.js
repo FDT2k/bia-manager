1ad9f9a0b88754e1111a77adf27f588b9ed69bc0
Fabien Karsegard <fabien@karsegard.ch>
2021-10-01 12:06:19 +0200

    react-crud as submodule

--- packages/kda-react-crud/src/example/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import '@/example/index.css'
-import App from '@/example/App'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-
