4ac38495f47ad062640666c4d3ccc94c826ac712
Fabien Karsegard <fabien@karsegard.ch>
2021-11-17 20:57:48 +0100

    Update index.js and index.js

--- packages/react-bia-manager/src/example/index.js
+++ packages/react-bia-manager/src/example/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import '@karsegard/react-core-layout/dist/style.css'
+import '@karsegard/bia-manager-theme/dist/style.css'
 import App from '@/example/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- packages/react-bia-manager/src/index.js
+++ packages/react-bia-manager/src/index.js
@@ -3,8 +3,7 @@ import {Provider as ViewProvider, useViewProvider} from "@/Context/BIAViews"
 import {Provider as TranslationProvider, useTranslation} from "@/Context/Translation"
 import {Provider as CustomListProvider, useCustomList} from "@/Context/CustomList"
 
-import '@karsegard/react-core-layout/dist/style.css'
-import '@karsegard/bia-manager-theme/dist/style.css'
+
 
 export {
     BIARouter,
