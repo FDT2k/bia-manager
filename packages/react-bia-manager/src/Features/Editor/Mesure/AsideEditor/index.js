90ead80d87b579f6b215f4cf6e380fba68470986
Fabien Karsegard <fabien@karsegard.ch>
2021-11-18 18:16:49 +0100

    Update index.js and index.js

---
+++ packages/react-bia-manager/src/Features/Editor/Mesure/AsideEditor/index.js
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import Button from '@/Components/Form/Button';
+import EditableSelect from '@/Components/Form/Editable/Select';
+import EditableTextInput from '@/Components/Form/Editable/TextInput';
+import EditableTextArea from '@/Components/Form/Editable/TextArea';
+import { safe_path } from '@karsegard/composite-js'
+import Field from '@/Components/Form/Fields';
+import { useFieldValues } from '@karsegard/react-hooks';
+
+
+import {useTranslation,useCustomList} from '@'
+
+export default props => {
+    const {t,oneDecimal,oneDecimalPct} = useTranslation();
+    const lists = useCustomList();
+
+    const {_handleClickSave,_handlePrint,data,onValuesChange} = props;
+
+    const { values,  inputProps, handleChange, replaceValues } = useFieldValues(data, { onValuesChange, usePath: true });    
+
+
+    useEffect(()=>{
+        replaceValues(data);
+    },[data])
+
+    return (
+
+        <>
+         <Button style={{ minWidth: '100%', width: '100%', maxWidth: '100%' }} tabIndex={33} onClick={_handleClickSave}>{t('Enregistrer')}</Button>
+                <Button tabIndex={44} className="btn--secondary" onClick={_ => _handlePrint()}>{t('Imprimer')}</Button>
+                <Field label={t("Examinateur")}>
+                    <EditableTextInput value={values.examinator} name="examinator" onChange={handleChange} />
+                </Field>
+                <Field label={t("Bio-impédancemètre")}>
+                    <EditableSelect {...inputProps('machine')} options={safe_path([], 'machines', lists)} />
+                </Field>
+                <Field label={t("Poids Idéal (%)")}>
+                    <div>{oneDecimal(values.ideal_weight)} ({oneDecimalPct(values.pct_ideal_weight)})</div>
+                </Field>
+                <Field label={t("BMI Actuel")}>
+                    <div>{values.bmi}</div>
+                </Field>
+                <Field label={t("BMI Reference")}>
+                    <EditableTextInput value={values.bmi_ref} name="bmi_ref" onChange={handleChange} />
+                </Field>
+                <Field label={t("Remarques / Interprétations")}>
+                    <EditableTextArea value={values.comments} name="comments" onChange={handleChange} />
+                </Field>
+        </>
+    )
+}
--- packages/react-bia-manager/src/Features/Editor/Mesure/index.js
+++ packages/react-bia-manager/src/Features/Editor/Mesure/index.js
@@ -1,19 +1,12 @@
 import { bem, getClasseNames } from '@karsegard/react-compose';
 import { useFieldValues } from '@karsegard/react-hooks';
 
-import Button from '@/Components/Form/Button';
-import EditableSelect from '@/Components/Form/Editable/Select';
-import EditableTextInput from '@/Components/Form/Editable/TextInput';
-import EditableTextArea from '@/Components/Form/Editable/TextArea';
-
-import Field from '@/Components/Form/Fields';
 
 import Printable from '@/Components/Printable';
 import PrintableReport from '@/Features/Editor/Mesure/PrintableReport';
 import Tabs, { Tab, TabList, TabPanel } from '@/Components/Tabs';
 
 
-import { safe_path } from '@karsegard/composite-js'
 import Serrement from './Serrement';
 
 
@@ -32,6 +25,7 @@ import Recap from './Recap'
 import { useCustomList, useTranslation } from '@'
 
 import BIAEditor from './BIAEditor'
+import AsideEditor from './AsideEditor';
 
 const LayoutFlexColumnWithArea = withGridArea(LayoutFlexColumn);
 const TabsWithArea = withGridArea(Tabs);
@@ -65,45 +59,24 @@ const Editor = props => {
     const {
         handleClickSave,
         className,
-        gender,
         handleGoBack,
-        handlePrint,
         handleChange: parentHandleChange,
         mesure,
         ...rest2 } = getClasseNames(__base_class, props)
+  
 
-
-    const {
-        recap,
-        list_dates,
-        mass_chart,
-        norm_chart,
-        get_current_bia,
-        machines,
-        sporttypes,
-        sportrates,
-        custom_lists,
-        refresh_editor_lists,
-        ...rest
-    } = rest2;
-
-
-    const current_bia = get_current_bia(['fmi', 'ffmi'])
     const _handleChange = (...args) => {
 
         parentHandleChange && parentHandleChange(...args);
     }
-    const { values, handleChangeValue, inputProps, handleChange, assignValues, replaceValues } = useFieldValues(mesure, { onValuesChange: _handleChange, usePath: true });
+
+
     const componentRef = useRef();
+
     const _handlePrint = useReactToPrint({
         content: () => componentRef.current
     })
 
-
-    /* update internal state if we change the prop */
-    useEffect(() => {
-        assignValues(mesure);
-    }, [mesure]);
 
 
     const _handleClickSave = e => {
@@ -116,10 +89,8 @@ const Editor = props => {
 
 
 
-    const result_columns = useMemo(() => ['norme', values.most_accurate_formula || 'kushner', 'gva'], [values.most_accurate_formula]);
+    const result_columns = useMemo(() => ['norme', mesure.most_accurate_formula || 'kushner', 'gva'], [mesure.most_accurate_formula]);
 
-    const lists = useCustomList();
-debugger;
     return (
         <MesureEditorLayout className={className}>
             <TabsWithArea tabIndexOffset={20} renderDisabledPanels={true} area="mesure-editor-main">
@@ -130,7 +101,7 @@ debugger;
 
                 </TabList>
                 <TabPanel>
-                    <BIAEditor data={values} onValuesChange={_handleChange}/>
+                    <BIAEditor data={mesure} onValuesChange={_handleChange}/>
                     <Container fit grow>
                         <ComparisonTable data={mesure.bia} columns={result_columns} />
                     </Container>
@@ -144,26 +115,7 @@ debugger;
 
             </TabsWithArea>
             <LayoutFlexColumnWithArea style={{ gap: '10px' }} area="mesure-editor-aside">
-                <Button style={{ minWidth: '100%', width: '100%', maxWidth: '100%' }} tabIndex={33} onClick={_handleClickSave}>{t('Enregistrer')}</Button>
-                <Button tabIndex={44} className="btn--secondary" onClick={_ => _handlePrint()}>{t('Imprimer')}</Button>
-                <Field label={t("Examinateur")}>
-                    <EditableTextInput value={values.examinator} name="examinator" onChange={handleChange} />
-                </Field>
-                <Field label={t("Bio-impédancemètre")}>
-                    <EditableSelect {...inputProps('machine')} options={safe_path([], 'machines', lists)} />
-                </Field>
-                <Field label={t("Poids Idéal (%)")}>
-                    <div>{oneDecimal(values.ideal_weight)} ({oneDecimalPct(values.pct_ideal_weight)})</div>
-                </Field>
-                <Field label={t("BMI Actuel")}>
-                    <div>{values.bmi}</div>
-                </Field>
-                <Field label={t("BMI Reference")}>
-                    <EditableTextInput value={values.bmi_ref} name="bmi_ref" onChange={handleChange} />
-                </Field>
-                <Field label={t("Remarques / Interprétations")}>
-                    <EditableTextArea value={values.comments} name="comments" onChange={handleChange} />
-                </Field>
+                <AsideEditor onValuesChange={_handleChange} handleClickSave={_handleClickSave} handlePrint={_handlePrint} data={mesure}/>
             </LayoutFlexColumnWithArea>
             <Printable ref={componentRef}>
                 {<PrintableReport />}
@@ -191,7 +143,6 @@ Editor.defaultProps = {
     },
     get_current_bia: _ => [],
     custom_lists: {},
-    gender: 'm',
     t: x => x,
 
 }
