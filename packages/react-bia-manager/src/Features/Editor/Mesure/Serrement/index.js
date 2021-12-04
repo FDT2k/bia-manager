b4af29332067f7f8993d48ef352bc4522c6738d9
Fabien Karsegard <fabien@karsegard.ch>
2021-12-04 12:21:09 +0100

    changed order of input on fds form

--- packages/react-bia-manager/src/Features/Editor/Mesure/Serrement/index.js
+++ packages/react-bia-manager/src/Features/Editor/Mesure/Serrement/index.js
@@ -38,31 +38,40 @@ export const Component = props => {
         <>
             <Grid
                 className="force-serrement"
-                templateColumns="repeat(3,1fr)"
-                templateRows="repeat(7,1fr)"
+                style={{maxWidth:'100%'}}
+                templateColumns="repeat(2,1fr) repeat(3,auto) repeat(2,1fr)"
+                templateRows="repeat(3,1fr)"
                 rowGap={10}
             >
                 <div className="header header--left"></div>
-                <div className="header">{t('Gauche')}</div>
-                <div className="header">{t('Droite')}</div>
                 <div className="header header--left">{t('Main dominante')}</div>
-                <div><input type="checkbox" {...checkboxProps('left.main')} /></div>
-                <div><input type="checkbox" {...checkboxProps('right.main')} /></div>
                 <div className="header header--left">{t('Mesure 1')}</div>
-                <div><input type="text" {...inputProps('left.data.0')} /></div>
-                <div><input type="text" {...inputProps('right.data.0')} /></div>
                 <div className="header header--left">{t('Mesure 2')}</div>
-                <div><input type="text" {...inputProps('left.data.1')} /></div>
-                <div><input type="text" {...inputProps('right.data.1')} /></div>
                 <div className="header header--left">{t('Mesure 3')}</div>
-                <div><input type="text" {...inputProps('left.data.2')} /></div>
-                <div><input type="text" {...inputProps('right.data.2')} /></div>
-                <div className="header header--left">{t('Moyenne des mesures')}</div>
-                <div>{oneDecimal(left_avg)} </div>
-                <div>{oneDecimal(right_avg)} </div>
+                <div className="header header--left">{t('Moyenne')}</div>
                 <div className="header header--left">{t('Normes')}</div>
-                <div>{left_norme} </div>
+
+                <div className="header">{t('Droite')}</div>
+                <div><input type="checkbox" {...checkboxProps('right.main')} /></div>
+                <div><input type="text" {...inputProps('right.data.0')} /></div>
+                <div><input type="text" {...inputProps('right.data.1')} /></div>
+                <div><input type="text" {...inputProps('right.data.2')} /></div>
+                <div>{oneDecimal(right_avg)} </div>
                 <div>{right_norme} </div>
+
+
+                <div className="header">{t('Gauche')}</div>
+                <div><input type="checkbox" {...checkboxProps('left.main')} /></div>
+
+                <div><input type="text" {...inputProps('left.data.0')} /></div>
+
+                <div><input type="text" {...inputProps('left.data.1')} /></div>
+
+
+                <div><input type="text" {...inputProps('left.data.2')} /></div>
+
+                <div>{oneDecimal(left_avg)} </div>
+                <div>{left_norme} </div>
 
             </Grid>
         </>
