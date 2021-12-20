78e6d8cdad8e20c7de4fb10cbfb5246eb8a9bea4
Fabien Karsegard <fabien@karsegard.ch>
2021-12-20 13:34:56 +0100

    Update index.js

--- packages/react-electron/src/App/Features/Search/index.js
+++ packages/react-electron/src/App/Features/Search/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { searchWithReduxModule, searchWithBackend,searchReduxModule } from '@karsegard/react-bia-manager';
 import { SearchPage, SearchFeature } from '@karsegard/react-bia-manager';
@@ -13,7 +13,11 @@ const Search = searchWithReduxModule(store)(searchWithBackend(SearchFeature))
 
 export default props => {
 
-    const {stats,db_name,exportToCSV} = useBackend();
+    const {stats,db_name,exportToCSV,fetch_stats} = useBackend();
+
+    useEffect(()=>{
+        fetch_stats();
+    },[])
     const search_count = useSelector(searchReduxModule.selectors.count);
     return (<SearchPage stats={stats} db_name={db_name} search_count={search_count}>
         <Search handlers={{
