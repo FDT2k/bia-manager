5fb77c0d32d7b34d06474d0d56e8c355991a7058
Fabien Karsegard <fabien@karsegard.ch>
2022-10-04 09:55:26 +0200

    Update translation.json, translation.json, and 3 more files...

--- packages/react-bia-manager/src/Features/About/index.js
+++ packages/react-bia-manager/src/Features/About/index.js
@@ -34,7 +34,7 @@ export const Page = props => {
                     <p>{t('Le logiciel est adaptable à toute structure en fonction des besoins et des contraintes techniques')}</p>
                     <p>{t('Pour un devis ou des informations, nous contacter sur')} <a href="#" onClick={e=>openURL(e,"mailto:contact@karsegard.ch")}>contact@karsegard.ch</a> </p>*/}
                     <h2>{t(`About BIM`)}</h2>
-                    <p>{t('BIM is an open-source app developed by ')}<a href="#" onClick={e=>openURL(e,"https://www.karsegard.ch")} target="_blank">{t('Karsegard Digital Agency SàRL')}</a></p>
+                    <p>{t('BIM is an open-source app developed by ')}<a href="#" onClick={e=>openURL(e,"https://www.karsegard.ch")} target="_blank"> {t('Karsegard Digital Agency SàRL')}</a></p>
                    
                     <h3>{t('Sponsors & Partners')}</h3>
                     <ul>
@@ -51,6 +51,13 @@ export const Page = props => {
                     <h3>{t('Customization')}</h3>
                     <p>{t('The software is adaptable to any structure and is customizable at will')}</p>
                     <p>{t('For an estimate or informations, contact us on')} <a href="#" onClick={e=>openURL(e,"mailto:contact@karsegard.ch")}>contact@karsegard.ch</a> </p>
+
+                    <h3>License MIT</h3>
+                    <p>Copyright © 2020, Karsegard Digital Agency Sàrl</p>
+                    <p>Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:</p>
+                    <p>The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.</p>
+                    <p>The Software is provided “as is”, without warranty of any kind, express or implied, including but not limited to the warranties of merchantability, fitness for a particular purpose and noninfringement. In no event shall the authors or copyright holders X be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.</p>
+                    <p>Except as contained in this notice, the name of the copyright holders shall not be used in advertising or otherwise to promote the sale, use or other dealings in this Software without prior written authorization from the copyright holders.</p>
                 </LayoutFlex>
             </Container>
         </Modal>
