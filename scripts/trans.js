9d4dec1e8030d8ff5dcd12da2592de369567b87f
Fabien Karsegard <fabien@karsegard.ch>
2022-11-24 11:36:01 +0100

    automated translation download

---
+++ scripts/trans.js
@@ -0,0 +1,25 @@
+var http = require('https');
+var fs = require('fs');
+
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  http.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);
+    });
+  });
+}
+
+const locales = [
+    'fr',
+    'en',
+    'de',
+    'it',
+    'es'
+]
+
+locales.map ( function (lang){
+    download("https://bim.karsegard.com/api/translations/bia_manager/translation/"+lang,"packages/electron/locales/"+lang+"/translation.json");
+    
+})
