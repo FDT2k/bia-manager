2bdec38df1ea4cc31453f96a04ac2806b83481f5
Fabien Karsegard <fabien@karsegard.ch>
2024-03-11 20:25:26 +0100

    Update Help.js, style.scss, and 14 more files...

--- packages/website/src/Help.js
+++ packages/website/src/Help.js
@@ -4,14 +4,14 @@ import React from 'react';
 
 import win1 from './images/win-1.png'
 import win2 from './images/win-2.png'
-import osx1 from './images/osx-1.png'
-import osx2 from './images/osx-2-fr.png'
-import osx3 from './images/osx-3.png'
-import osx4 from './images/osx-4.png'
-import osx5 from './images/osx-5.png'
-import osx6 from './images/osx-6.png'
+import osCs2 from './images/osx-catalina-step2-fr.png'
+import osCs3 from './images/osx-catalina-step3-fr.png'
+// import osx3 from './images/osx-3.png'
+import osxSs1 from './images/osx-sonoma-step1-fr.png'
+import osxSs2 from './images/osx-sonoma-step2-fr.png'
+import osxSs3 from './images/osx-sonoma-step3-fr.png'
 
-console.log(osx2)
+console.log(osCs3)
 export default props => {
     const {params} = props;
 
@@ -47,32 +47,24 @@ export default props => {
                     <header>
                         <h2>Mac OS</h2>
                     </header>
-                    {/* <article className="flex flex-row just-between align-center">
-                        <img src={osx4} alt="Première étape de téléchargement" />
-                        <p>
-                        Une fois téléchargé, le logiciel peut indiquer qu'il n'est pas signé. Vous pouvez cliquer sur OK et vous rendres dans les paramêtres de votre mac et cliquer sur l'icone sécurité
-
-                        </p>
-                    </article> */}
-
+ 
                     <article className="flex flex-row just-between align-center">
-                        <img src={osx4} alt="Première étape de téléchargement" />
+                        <img src={osxSs1} alt="Première étape de téléchargement" />
                         <p>
-                        Une fois téléchargé, le logiciel peut indiquer qu'il n'est pas signé. Vous pouvez cliquer sur OK et vous rendres dans les paramêtres de votre mac et cliquer sur l'icone sécurité
-
+                        Une fois téléchargé, faites glisser l'application "BIA-Manager" dans le dossier "Applications".
                         </p>
                     </article>
 
                     <article className="flex flex-row just-between align-center">
-                        <img src={osx6} alt="Première étape de téléchargement" />
+                        <img src={osxSs2} alt="Deuxième étape de téléchargement" />
                         <p>
-                        Une fois téléchargé, le logiciel peut indiquer qu'il n'est pas signé. Vous pouvez cliquer sur OK et vous rendres dans les paramêtres de votre mac et cliquer sur l'icone sécurité
+                        Le logiciel peut indiquer qu'il n'est pas signé. Vous pouvez cliquer sur OK et vous rendres dans les paramêtres de votre mac et cliquer sur l'icone sécurité
 
                         </p>
                     </article>
                     
                     <article className="flex flex-row just-between align-center">
-                        <img src={osx5} alt="Deuxième étape de téléchargement" />
+                        <img src={osxSs3} alt="Troisième étape de téléchargement" />
                         <p>
                         En bas de cette fenêtre vous aurez un texte vous indiquant la dernière application ayant tenté de s'ouvrir, vous pouvez cliquer sur "ouvrir quand même".
                         </p>
@@ -83,14 +75,20 @@ export default props => {
                         <h2>Mac OS - Ancienne méthode</h2>
                     </header>
                     <article className="flex flex-row just-between align-center">
-                        <img src={osx1} alt="Première étape de téléchargement" />
+                        <img src={osxSs1} alt="Première étape de téléchargement" />
                         <p>
-                        Une fois téléchargé, le logiciel peut indiquer qu'il n'est pas signé. Vous pouvez cliquer sur OK et vous rendres dans les paramêtres de votre mac et cliquer sur l'icone sécurité
+                        Une fois téléchargé, faites glisser l'application "BIA-Manager" dans le dossier "Applications".
+                        </p>
+                    </article>
+                    <article className="flex flex-row just-between align-center">
+                        <img src={osCs2} alt="Deuxième étape de téléchargement" />
+                        <p>
+                        Le logiciel peut indiquer qu'il n'est pas signé. Vous pouvez cliquer sur OK et vous rendres dans les paramêtres de votre mac et cliquer sur l'icone sécurité
 
                         </p>
                     </article>
                     <article className="flex flex-row just-between align-center">
-                        <img src={osx2} alt="Deuxième étape de téléchargement" />
+                        <img src={osCs3} alt="Troisième étape de téléchargement" />
                         <p>
                         En bas de cette fenêtre vous aurez un texte vous indiquant la dernière application ayant tenté de s'ouvrir, vous pouvez cliquer sur "ouvrir quand même".
                         </p>
